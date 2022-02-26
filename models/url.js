cf622ec8e26aa0e46b227acfd7a2b8efc76b5283
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-02-26 14:58:10 +0200

    added auto incrament to url schmea & some changes

--- models/url.js
+++ models/url.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose')
+autoIncrement = require('mongoose-auto-increment');
 
+/* Database Connection */
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("Connected to DB")
+});
+
+/* Connection instance */ 
+let connection = mongoose.createConnection(process.env.MONGO_URI)
+
+/* Initialize autoincrament with connection */ 
+autoIncrement.initialize(connection);
+
+/* Created a url schema */ 
 const urlSchema = new mongoose.Schema({
     original_url: {type: String, required:  true},
-    short_url: {type: Number, required: true}
+    short_url: Number
 })
 
-module.exports = mongoose.model('Url', urlSchema)
+/* exported url model & excuted autoincrament for url schema */ 
+urlSchema.plugin(autoIncrement.plugin, { model: 'Url', field: 'short_url' });
+module.exports = connection.model('Url', urlSchema)
 

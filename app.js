992f1028a71707bf1e2842325134a0a0a422de50
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-02-26 15:19:40 +0200

    adde get method

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-const Url = require('./models/url')
+const Url = require('./models/url');
+const { json } = require('body-parser');
+const { response } = require('express');
 
 
 // Middleware
@@ -22,14 +24,35 @@ app.get("/", (req, res) => {
 
 
 
-app.post('/api/short/new', (req, res) => {
-    let newUrl = new Url({original_url:  req.body.original_url})
-    newUrl.save((err,data)=>{
-        if (err) res.status(200).json({error : err.message})
-        res.status(200).json(data)
+app.post('/api/shorturl/new', (req, res) => {
+    let reqUrl = req.body.original_url;
+    let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
+
+    if (!reqUrl.match(urlRegex)) return res.status(401).json({ error: "Invalid URL!" })
+
+    let objUrl = new Url({ original_url: reqUrl });
+    objUrl.save((err, data) => {
+        if (err) res.status(200).json({ error: err.message });
+        res.status(200).json(data);
     })
-    
+
 })
+
+
+app.get('/api/shorturl/:input', (req, res) => {
+    let input = req.params.input;
+
+    Url.findOne({ short_url: parseInt(input) }, (err, data) => {
+        console.log(data)
+        if (err && data == undefined) return res.status(401).json({ error: "URL Not found!" })
+        res.writeHead(302, { 'Location': data.original_url }).end()
+        return res;
+
+    })
+
+})
+
+
 
 // Server init 
 app.listen(process.env.SERVER_PORT, () => {
